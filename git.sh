ab31080c1768a1d88c00d524bd195fc179a2b5aa
yip <yipkwunleong@gmail.com>
2022-06-04 13:49:06 +0800

    init

--- git.sh
+++
@@ -1,8 +0,0 @@
-export BRANCH_NAME=$1
-git init
-git remote add origin https://github.com/yip-kl/airflow_example.git
-git branch $BRANCH_NAME
-git branch -M $BRANCH_NAME
-git add .
-git commit -m "$2"
-git push -u origin $BRANCH_NAME
