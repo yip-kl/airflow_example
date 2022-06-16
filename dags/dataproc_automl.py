1e9b8fa22c0ea57c0a37a5ea99d30c1e76ecbc47
Julian Yip <julian.yip@livibank.com>
2022-06-16 17:48:49 +0800

    update dataproc batch arg

--- dags/dataproc_automl.py
+++ dags/dataproc_automl.py
@@ -7,6 +7,7 @@ from airflow.models import Variable
 from datetime import datetime
 import pendulum
 import arrow
+from google.cloud import dataproc_v1
 
 local_tz = pendulum.timezone('Asia/Hong_Kong')
 
@@ -21,7 +22,42 @@ default_args = {
 project_id = 'adroit-hall-301111'
 dataset_id = 'demo'
 region = 'us-central1' ## try passing this as default args?
-       
+
+# Define Dataproc batch, the DataprocCreateBatchOperator rides on the create_batch method (refer to batch_controller) of the dataproc_v1 library,
+# which in turns requires a batch class to be defined, see below
+# https://github.com/googleapis/python-dataproc/blob/main/google/cloud/dataproc_v1/services/batch_controller/client.py
+# https://github.com/googleapis/python-dataproc/blob/main/google/cloud/dataproc_v1/types/batches.py
+dataproc_batch = dataproc_v1.Batch()
+batch_id = f'batch-{round(arrow.utcnow().timestamp())}'
+batch_spec = {
+  "pyspark_batch": {
+    "jar_file_uris": [
+      "gs://spark-lib/bigquery/spark-bigquery-with-dependencies_2.12-0.25.0.jar"
+    ],
+    "main_python_file_uri": "gs://dataproc-staging-us-central1-712368347106-boh5iflc/notebooks/jupyter/MLinPython/pyspark/pyspark_batch.py"
+  },
+  "labels": {},
+  "name": f"projects/adroit-hall-301111/locations/us-central1/batches/{batch_id}",
+  "runtime_config": {
+    "properties": {
+      "spark.executor.instances": "2",
+      "spark.driver.cores": "4",
+      "spark.executor.cores": "4",
+      "spark.app.name": f"projects/adroit-hall-301111/locations/us-central1/batches/{batch_id}"
+    }
+  },
+  "environment_config": {
+    "execution_config": {
+      "subnetwork_uri": "default"
+    }
+  }
+}
+def set_attr_from_dict(class_obj, dictionary):
+    for k, v in dictionary.items():
+        setattr(class_obj, k, v)
+
+dataproc_batch = set_attr_from_dict(dataproc_batch, batch_spec)
+
 with DAG('dataproc_automl', description='',
         schedule_interval='0 22 * * *',
         default_args=default_args) as dag:
@@ -48,11 +84,8 @@ with DAG('dataproc_automl', description='',
       task_id='feature_engineering',
       project_id=project_id,
       region=region,
-      batch_id=f'batch_{round(arrow.utcnow().timestamp())}',
-      batch={
-        "labels": {},
-        "name": "projects/adroit-hall-301111/locations/us-central1/batches/batch-abc"
-      }
+      batch_id=batch_id,
+      batch=dataproc_batch
     )
 
     t3 = BigQueryTableExistenceSensor(
@@ -80,6 +113,6 @@ with DAG('dataproc_automl', description='',
         }
     )
 
-    t2 >> t3 >> t4
+    t1 >> t2 >> t3 >> t4
 
 
